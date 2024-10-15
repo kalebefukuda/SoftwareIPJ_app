242e02649c108928a38fb489fa7be63f30a0c42a
João Duarte <jpdx2010@hotmail.com>
2024-10-15 19:52:16 -0300

    - Adição do menu lateral e suas funções de tema e logoff - Ajustes no CustomDrawer para manter o estado do interruptor de tema ao navegar. - Corrigido bug de desatualização do tema ao voltar para a tela home. - Alteração do tema de AUTOMÁTICO DO SISTEMA para MUDANÇA MANUAL

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,11 +2,25 @@ import 'package:flutter/material.dart';
 import '../widgets/card_register.dart';
 import '../widgets/card_members.dart';
 import '../widgets/card_report_home.dart';
-import '../widgets/card_count_members.dart'; // Importe o novo widget
+import '../widgets/card_count_members.dart';
+import '../widgets/custom_drawer.dart';
+import '../screens/report.dart'; // Importe o ReportScreen
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatefulWidget {
+  final Function(bool)? onThemeToggle; // Torne onThemeToggle opcional
+  final bool isDarkMode;
 
+  const HomeScreen({
+    super.key,
+    this.onThemeToggle, // Agora opcional
+    this.isDarkMode = false, // Valor padrão para isDarkMode
+  });
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,15 +28,23 @@ class HomeScreen extends StatelessWidget {
         toolbarHeight: 90,
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
         scrolledUnderElevation: 0,
-        leading: IconButton(
-          onPressed: () {
-            Scaffold.of(context).openDrawer(); // Abre o menu lateral
+        leading: Builder(
+          builder: (context) {
+            return IconButton(
+              onPressed: () {
+                Scaffold.of(context).openDrawer(); // Abre o menu lateral
+              },
+              icon: Icon(
+                Icons.menu,
+                size: 30,
+                color: Theme.of(context).iconTheme.color,
+              ),
+              splashColor: Colors.transparent, // Remove a sombra ao clicar
+              highlightColor: Colors.transparent, // Remove o destaque ao clicar
+              focusColor: Colors.transparent, // Remove o destaque ao focar
+              hoverColor: Colors.transparent, // Remove o destaque ao passar o mouse por cima
+            );
           },
-          icon: Icon(
-            Icons.menu,
-            size: 30,
-            color: Theme.of(context).iconTheme.color,
-          ),
         ),
         centerTitle: true,
         title: Row(
@@ -37,30 +59,44 @@ class HomeScreen extends StatelessWidget {
           ],
         ),
       ),
-      body: const SingleChildScrollView(
-        padding: EdgeInsets.all(20.0),
+      drawer: CustomDrawer(
+        onThemeToggle: widget.onThemeToggle ?? (bool value) {},
+        isDarkMode: widget.isDarkMode,
+      ), // Utilize o CustomDrawer como menu lateral
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(20.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            SizedBox(height: 10),
+            const SizedBox(height: 10),
             // Card de cadastro de membros, ocupando toda a largura
-            CardRegister(),
-            SizedBox(height: 29),
+            const CardRegister(),
+            const SizedBox(height: 29),
             // Cards de membros e relatório na mesma linha, ocupando cada um metade da largura do card acima
             Row(
               children: [
-                Expanded(
+                const Expanded(
                   child: CardMembers(),
                 ),
-                SizedBox(width: 29), // Espaço entre os dois cards
+                const SizedBox(width: 29), // Espaço entre os dois cards
                 Expanded(
-                  child: CardReport(),
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const Report(), // Navega para a tela de relatório ao clicar no CardReport
+                        ),
+                      );
+                    },
+                    child: const CardReport(),
+                  ),
                 ),
               ],
             ),
-            SizedBox(height: 29),
+            const SizedBox(height: 29),
             // Adicionando o card de contagem de membros
-            MembersCountCard(),
+            const MembersCountCard(),
           ],
         ),
       ),
